70df3f922f0fb83b9ccb4e674442c089fe6e8235
daviddeek <109833309+daviddeek@users.noreply.github.com>
2024-01-04 11:54:28 -0600

    Update Tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,6 +18,15 @@ export default defineConfig({
 					autogenerate: { directory: 'research' },
 				},
 			],
+			head: [{
+				tag: 'meta',
+				attributes: {
+					name: 'ahrefs-site-verification',
+					content: '43e11f8b01ac09eae0c1d5137e5472c4fbc683844b5349b3aaa4efc23d3fb0b2',
+				}
+			},
+			{ tag: 'meta', attribute: { name: "google-site-verification", content: "JcdPH-8wgLFOXHDEMQ_zLE2mj7pUy9OgDzzQxNBAo1Q" } }
+			],
 			customCss: ['./src/tailwind.css'],
 			defaultLocale: 'root', // optional
 			locales: {
